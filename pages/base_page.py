a2b0ba159e12a15b62729b2e7eb63b025e59d296
Pelmen323 <nozovka9@mail.ru>
2021-11-18 16:56:57 +0300

    Added methods: is_element_present for Base Page, should_be_login_link for Main Page, added new test should_see_login_link

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,23 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage:
     """
     Class for initializing Page object\n
     Receives browser and url during initialization\n
     Has in-built method open(self)
     """
-    def __init__(self, browser: object, url: str) -> None:
+    def __init__(self, browser: object, url: str, timeout: int = 10) -> object:
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, locator_type, selector):
+        try:
+            self.browser.find_element(locator_type, selector)
+        except NoSuchElementException:
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -3,12 +3,15 @@ from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
-     """
+    """
     Class for Main Page\n
     Inherits init and open(self) methods\n
-    Own methods: 
+    Own methods:
     - go_to_login_page - finds element by login link
     """
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not found"
